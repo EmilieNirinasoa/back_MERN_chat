caeac70e651f027bcb3f30be4bf856b56baf7145
EmilieNirinasoa <fanomezanaemilienirinasoa@gmail.com>
2024-11-25 16:56:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const dotenv=require('dotenv')
+const app=express()
+dotenv.config()
+app.get("/",(req,res)=>{
+    res.send("api is running")
+})
+
+
+
+
+const PORT=process.env.PORT || 5000;
+app.listen( PORT,
+    ()=>{
+        console.log('running on port 5000....')
+    }
+)
